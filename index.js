a7e183bdaf0dbc2ecbfaed3dc060544e2d9500bf
Axel <eldrank@gmail.com>
2022-09-02 00:00:20 +0200

    make consecutive calculation work

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const numbers = document.querySelectorAll(".num");
 const operators= document.querySelectorAll(".op");
+
 const clearBtn = document.querySelector(".clear");
+const decimalBtn = document.querySelector(".dot");
+const delBtn = document.querySelector(".del");
 
 const displayMemory = document.querySelector(".memoryValue");
 const displayValue = document.querySelector(".displayValue");
@@ -40,8 +43,6 @@ function operate(op, a, b){
         case "/":
             return roundNumber(divide(a, b));
       }
-
-
 }
 
 function handleNumber(num){
@@ -51,9 +52,41 @@ function handleNumber(num){
 }
 
 function handleOperator(op){
-    operator = op;
-    previousNum = currentNum;
-    currentNum = "";
+    if (operator === ""){
+        operator = op;
+        previousNum = currentNum;
+        currentNum = "";
+    }else {
+        currentNum = operate(operator, Number(previousNum), Number(currentNum));
+        previousNum = currentNum;
+        currentNum = ""
+        operator = op;
+    }
+    
+    
+}
+
+function roundNumber(num){
+    return Math.round(num * 1000) / 1000;
+}
+
+function addDecimal(){
+    if(!currentNum.includes(".")){
+        currentNum += ".";
+    }
+}
+
+function deleteNum(){
+    currentNum = currentNum.toString().slice(0, -1);
+    displayValue.textContent = currentNum;
+}
+
+function clearDisplay(){
+    currentNum = ""
+    previousNum = ""
+    operator = ""
+    displayValue.innerHTML = "0";
+    displayMemory.innerHTML = "";
 }
 
 numbers.forEach(number => {
@@ -76,50 +109,11 @@ operators.forEach(op => {
                 displayValue.textContent = "Too long";
             }else{
                 displayValue.textContent = currentNum;
-            }
-            
-        }
-        
+            }    
+        }    
     })
 })
 
-function roundNumber(num){
-    return Math.round(num * 1000) / 1000;
-}
-
-/* numbers.forEach(number => {
-    number.addEventListener("click", () => {
-        if (operator === ""){
-            firstNum += number.id;
-            displayValue.textContent = "";
-            displayMemory.innerHTML = "";
-            displayValue.append(firstNum);
-        }else {
-            secondNum += number.id;
-            displayValue.textContent = "";
-            displayMemory.textContent = `${firstNum} ${operator} ${secondNum}`;
-        };
-    });
-});
-
-operators.forEach(op => {
-    op.addEventListener("click", ()=> {
-        if (op.id !== "="){
-            operator = op.id;
-        }else {
-            displayValue.textContent = operate(operator, parseInt(firstNum), parseInt(secondNum));
-            firstNum = "";
-            secondNum = ""
-            operator = ""
-            displayValue.innerHTML = "";
-        }
-    })
-}) */
-
-clearBtn.addEventListener("click", () => {
-    currentNum = ""
-    previousNum = ""
-    operator = ""
-    displayValue.innerHTML = "0";
-    displayMemory.innerHTML = "";
-});
+decimalBtn.addEventListener("click", () => addDecimal());
+delBtn.addEventListener("click", () => deleteNum());
+clearBtn.addEventListener("click", () => clearDisplay());
